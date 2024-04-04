c3588cd326eec2c7002216ebbc55043643f7cd94
Amália Coelho <86680741+amalia-coelho@users.noreply.github.com>
2024-04-04 18:07:03 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -9,8 +9,9 @@
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <link rel="stylesheet" href="css/cadastro.css">
+    <link rel="stylesheet" href="css/login-cadastro.css">
     <link rel="stylesheet" href="css/footer.css">
 	<script src="js/jquery-3.6.0.min.js"></script>
 	
@@ -20,31 +21,29 @@
     <?php include("php/navbar.php"); ?>
     <main>
         <div class="containerF">
-            <div class="wrapperF">
-                <div class="title">
-                    <h1>Cadastro</h1>
-                </div>
-                <div class="form">
-                    <form id="form_cadastro" method="post" enctype="multipart/form-data">
-                        <div class="row">
-                            <input type="text" name="user" placeholder="Nome completo">
-                        </div>
-                        <div class="row">
-                            <input type="number" name="tel" placeholder="Telefone">
-                        </div>
-                        <div class="row">
-                            <input type="text" name="login" placeholder="E-mail">
-                        </div>
-                        <div class="row">
-                            <input type="text" name="password" placeholder="Senha">
-                        </div>
-                        <span></span> <!-- retorno ajax-->
-                        <div class="row button">
-                            <button type="submit" id="entrar" class="btn-join">Cadastrar</button>
-                        </div>
-                        <div class="signup-link">Not a member? <a href="login.php">Akame | login</a></div>
-                    </form> 
-                </div>
+            <div class="title">
+                <h1>Cadastro</h1>
+            </div>
+            <div class="form">
+                <form id="form_cadastro" method="post" enctype="multipart/form-data">
+                    <div class="row">
+                        <input type="text" name="user" placeholder="Nome completo">
+                    </div>
+                    <div class="row">
+                        <input type="number" name="tel" placeholder="Telefone">
+                    </div>
+                    <div class="row">
+                        <input type="text" name="login" placeholder="E-mail">
+                    </div>
+                    <div class="row">
+                        <input type="text" name="password" placeholder="Senha">
+                    </div>
+                    <span></span> <!-- retorno ajax-->
+                    <div class="row button">
+                        <button type="submit" id="entrar" class="btn-join">Cadastrar</button>
+                    </div>
+                    <div class="signup-link">Not a member? <a href="login.php">Akame | login</a></div>
+                </form> 
             </div>
         </div>
     </main>
