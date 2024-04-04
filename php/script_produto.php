733545d7d220cae34cc7f6b7a1c4e741610be993
Amália Coelho <86680741+amalia-coelho@users.noreply.github.com>
2024-04-04 18:06:30 -0300

    Update script_produto.php

--- php/script_produto.php
+++ php/script_produto.php
@@ -12,7 +12,7 @@ session_start();
     $nomeFinal_2 = time().uniqid().$ext_2;
 
 // Verificando os campos preenchidos
-if($_POST['nm_produto'] == "" or $_POST['nr_valor'] == "" or $_POST['ds_produto'] == "" or $_POST['nr_estoque'] == ""){
+if($_POST['nm_produto'] == "" or $_POST['nr_valor'] == "" or $_POST['ds_produto'] == ""){
     echo "Preencha todos os campos!";
 }else{
     // Consultando se ja existe um produto com mesmo nome
@@ -26,15 +26,14 @@ if($_POST['nm_produto'] == "" or $_POST['nr_valor'] == "" or $_POST['ds_produto'
         if($ext_1 == '.jpg' || $ext_1 == 'jpeg' || $ext_1 == '.png' || $ext_2 == '.jpg' || $ext_2 == 'jpeg' || $ext_2 == '.png'){
             try{
                 if (move_uploaded_file($img_1['tmp_name'], '../img/'.$nomeFinal_1) && move_uploaded_file($img_2['tmp_name'], '../img/'.$nomeFinal_2)) {
-                $stmt = $conn->prepare('INSERT INTO tb_products(nm_produto, vl_produto, ds_produto, img_1, img_2, nr_estoque) VALUES(:nm_produto, :nr_valor, :ds_produto, :img_1, :img_2, :nr_estoque)');
+                $stmt = $conn->prepare('INSERT INTO tb_products(nm_produto, vl_produto, ds_produto, img_1, img_2) VALUES(:nm_produto, :nr_valor, :ds_produto, :img_1, :img_2)');
                 $result = $stmt->fetch(PDO::FETCH_ASSOC);   
                 $stmt->execute(array(
                     ':nm_produto' => $_POST['nm_produto'],
                     ':nr_valor' => $_POST['nr_valor'],
                     ':ds_produto' => $_POST['ds_produto'],
                     ':img_1' => $nomeFinal_1,
-                    ':img_2' => $nomeFinal_2,
-                    ':nr_estoque' => $_POST['nr_estoque']
+                    ':img_2' => $nomeFinal_2
                 ));
                 // Atualizando a pagina
                 echo "<meta HTTP-EQUIV='refresh' CONTENT='0'>";
